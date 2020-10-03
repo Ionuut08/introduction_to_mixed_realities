f13c1168a54502282dea629892072edacb4fe925
Ionut Feraru <ionut.feraru@info.uaic.ro>
2020-10-03 17:34:02 +0300

    First laboratory

---
+++ First_laboratory/Assets/scripts/cameraBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject object1;
+    public GameObject object2;
+    public Animator animate;
+
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance (object1.transform.position, object2.transform.position);
+        
+        if (distance <= 1)
+        {
+           animate.SetBool("cameraIsClose", true);
+        } else {
+            animate.SetBool("cameraIsClose", false);
+        }
+    }
+}
